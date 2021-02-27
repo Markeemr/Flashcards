af0352409dc4ea1e551375e83b22a2855d76d7c1
Warith Balogun <warithb2001@gmail.com>
2021-02-26 19:16:50 -0500

    Initial Commit

---
+++ Shared/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Shared
+//
+//  Created by Warith Balogun on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
